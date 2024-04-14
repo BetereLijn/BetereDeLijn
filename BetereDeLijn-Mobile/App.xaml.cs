f4a6b6e2f670c4ff275d6fe2a25d15193532b333
Ilyaan Ali <ali.ilyaan786@gmail.com>
2024-04-14 23:02:05 +0200

    Add project files.

---
+++ BetereDeLijn-Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BetereDeLijn_Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
