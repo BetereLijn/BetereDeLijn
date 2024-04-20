288d862191e6a8da6cddb3db56164616fc24bc65
Daya Huybrechts <daya@huybrechts.one>
2024-04-20 16:45:25 +0200

    eerste api call added

--- BetereDeLijn-Mobile/MainPage.xaml.cs
+++ BetereDeLijn-Mobile/MainPage.xaml.cs
@@ -1,7 +1,13 @@
-﻿namespace BetereDeLijn_Mobile
+﻿using System.Reflection.Emit;
+using System;
+using System.Text.Json;
+using System.Net.Http;
+
+namespace BetereDeLijn_Mobile
 {
     public partial class MainPage : ContentPage
     {
+        HttpClient client = new HttpClient();
         int count = 0;
 
         public MainPage()
@@ -19,6 +25,25 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        private async void testprocedure(object sender, EventArgs e)
+        {
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "c94acbf3136844b08c1fa4281955241b");
+            //hier komt de url
+            var uri = "https://api.delijn.be/DLKernOpenData/api/v1/api";
+
+            var response = await client.GetStringAsync(uri);
+            using (JsonDocument document = JsonDocument.Parse(response))
+            {
+                test.Text = "";
+                JsonElement root = document.RootElement;
+                JsonElement links = root.GetProperty("links");
+                foreach (JsonElement link in links.EnumerateArray())
+                {
+                    test.Text += link.GetProperty("url");
+                }
+            }
         }
     }
 
